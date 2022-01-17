f0a809a8ea39d584fb8d5ab6d6534627b0331be4
ajohnson2205 <david.johnsoniv@gmail.com>
2022-01-16 16:28:59 -0800

    Fixing about page.

--- front/src/components/FeaturetteAbout.js
+++ front/src/components/FeaturetteAbout.js
@@ -32,7 +32,17 @@ class FeaturetteAbout extends Component {
       <div className="col-md-7">
         <h2 className="featurette-heading-baby">About the artist.</h2>
         <p className="lead">
-          Monica Johnson was born and raised on the beautiful Oregon coast, a place she still calls home with her husband, Ryan, her dogs Peanut and Ruby, and her cat Iris.
+
+
+          Hello! My name is Monica Deatherage, and I am a freelance illustrator based on the Oregon coast. 
+
+          I create colorful, whimsical illustrations. I love color and texture; and work with an array of materials, most often watercolor, colored pencil, and graphite. With my art I explore the world through the eyes of the kid inside me; a dorky, sweet, animal-loving kid who wants to find all the good in the world. I try to capture the magic, adventure, and mystery of childhood. 
+
+          I found a love of art as a young kid, and have always been especially drawn to picture books. With a kindergarten teacher for a mother and and artist for a father, I grew up in an atmosphere of learning and creativity. One of our favorite family trips was going to an enormous bookstore in Portland, Oregon (the big city for us!) called Powell's Books. I would find a cozy corner to plant myself, and spend the next few hours reading books, completely mesmerized by the beautiful illustrations. Even now, a mother myself, I can't get enough of reading picture books to my daughter; still finding myself drawn into stories by the beautiful artwork.
+
+          I have taken classes at Portland Community College and Southwestern Oregon Community College, but am mostly a self-taught artist. 
+
+          If you are interested in my work I would love to hear from you! You can email me at monica@moesart.net, I would be so happy to hear about your project!
         </p>
         <p className="lead">
           Monica works in a variety of mediums including prismacolor, watercolor, gouache,  and acrylic.  She attended Portland Community College and Pacific University where she studied figure drawing, sculpture, design, and painting.
